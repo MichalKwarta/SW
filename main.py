62385010a6f1ebb4fe817ea52927b1da36153025
mkwarta <Michu1164@gmail.com>
2022-01-20 08:49:53 +0100

    update

--- main.py
+++ main.py
@@ -9,26 +9,17 @@ from time import sleep
 import Adafruit_BBIO.GPIO as GPIO
 
 
-
-
-def silnikrobibrr(enA='P9_21'):
-
+def silnikrobibrr(enA='P9_23'):
     GPIO.output(enA, GPIO.HIGH)
     print("BRRRR")
-    sleep(2)
+    sleep(0.5)
     GPIO.output(enA, GPIO.LOW)
-
-
-
-
-
 
 
 def countDots():
     camera = cv2.VideoCapture(0)
     _, frame = camera.read()
-    cv2.imwrite("nowe.jpg",frame)
-
+    cv2.imwrite("nowe.jpg", frame)
     _, _, red = cv2.split(frame)
     _, threshh = cv2.threshold(red, 50, 255, cv2.THRESH_BINARY)
     contours = cv2.findContours(threshh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
@@ -50,43 +41,37 @@ def establishConnection(db):
         exit()
     return conn, cur
 
+
 def printhelp():
-        print('''L o S o W a Ń s K O
+    print('''L o S o W a Ń s K O
           q L U- losuj ziarno i wygeneruj liczbę z zakresu <L;U>
           w N L U - pobierz z bazy danych ostanie N wyników i wygeneruj liczbę z zakresu <L;U>  
           h - help
           e - exit
           ''')
-        
-        
+
+
 if __name__ == '__main__':
     db = r'Kropeczki.db'
 
-    #pins setup
-    enA = "P9_21"
-    in1 = "P9_23"
+    # pins setup
+    in1 = "P9_21"
+    pwm = "P9_23"
     in2 = "P9_25"
-    GPIO.setup(enA, GPIO.OUT)
+    GPIO.setup(pwm, GPIO.OUT)
     GPIO.setup(in1, GPIO.OUT)
     GPIO.setup(in2, GPIO.OUT)
-    
-    GPIO.output(enA, GPIO.LOW)
+
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
-    
-    
-    #lcd setup
+
+    # lcd setup
     cols = 20
-    rows = 4
+    rows = 2
 
     lcd = liquidcrystal_i2c.LiquidCrystal_I2C(0x27, 2, numlines=rows)
     lcd.backlight()
 
-   
-    
-    
-    
-    
     printhelp()
 
     while True:
@@ -99,24 +84,25 @@ if __name__ == '__main__':
             exit(1)
 
         if choice == 'Q' and len(args) == 2:
+            lcd.clear()
             conn, cur = establishConnection(db)
             silnikrobibrr()
             dots = countDots()
-            result = "ilosc kostek " + str(dots)
+            result = "Ile " + str(dots)
             print(result)
             seed(dots)
             rand_number = randint(args[0], args[1])
             print(rand_number)
-            lcd.printline(0, result.center(cols))
-            lcd.printline(1,rand_number.center(cols ))
-            
-            
-            cur.execute("Insert into Kropeczki (suma) values ("+ str(dots)+ ");")
+            lcd.printline(0, str(result).center(cols))
+            lcd.printline(1, str(rand_number).center(cols))
+
+            cur.execute("Insert into Kropeczki (suma) values (" + str(dots) + ");")
             conn.commit()
             conn.close()
 
 
         elif choice == 'W' and len(args) == 3:
+            lcd.clear()
             conn, cur = establishConnection(db)
             cur.execute("select suma from Kropeczki order by id desc limit " + str(args[0]))
             records = cur.fetchall()
@@ -124,25 +110,29 @@ if __name__ == '__main__':
             build_string = ""
             for i, x in enumerate(records):
                 print(i, int(x[0]))
-                build_string+=" {}".format(str(x[0]))
-            
-            split_string = [build_string[i:i+20] for i in range(0, len(build_string), 20)]
-            
+                build_string += " {}".format(str(x[0]))
+
+            split_string = [build_string[i:i + 20] for i in range(0, len(build_string), 20)]
+
             seed(total)
             rand_number = randint(args[1], args[2])
-            if len(split_string)<=4:
-                
-                for i,string in enumerate(split_string):
-                    lcd.printline(i,string)
+            print("wylosowano ",rand_number)
+            if len(split_string) <= 1:
+
+                for i, string in enumerate(split_string):
+                    lcd.printline(i, string)
+                lcd.printline(1,str(rand_number).center(cols))
+
             else:
-                lcd.printline(0,"za duzy output")
-                lcd.printline(1,str(rand_number))
+                lcd.printline(0, "za duzy output")
+                lcd.printline(1, str(rand_number))
+
             conn.close()
-            
-            
+
+
         elif choice == "H" or "HELP":
             printhelp()
-            
+
         elif choice == 'E':
             exit();
         else:
